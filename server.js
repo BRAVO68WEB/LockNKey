2143c7c13a16ee2e5d47b3495327448be8a9b2ec
apex <me@apexweb.me>
2021-10-06 13:21:12 +0530

    remigrated

--- server.js
+++ server.js
@@ -1,40 +1,40 @@
 const fastify = require("fastify")({
-  logger: {
-    level: "info",
-    file: "./requests.log", // will use pino.destination()
-  },
+	logger: {
+		level: "info",
+		file: "./requests.log", // will use pino.destination()
+	},
 });
 const fs = require("fs");
+fastify.register(require("point-of-view"), {
+	engine: {
+		ejs: require("ejs"),
+	},
+});
 
 fastify.register(require("fastify-formbody"));
 
 fastify.get("/hello", function (req, res) {
-  res.send({ message: "LockNKey Server" });
+	res.send({ message: "LockNKey Server" });
 });
 
 fastify.get("/", function (req, res) {
-  const bufferIndexHtml = fs.readFileSync("index.html");
-  res.type("text/html").send(bufferIndexHtml);
+	res.view("/views/index.ejs", { text: "text" });
 });
 
 fastify.get("/encode", function (req, res) {
-  const bufferIndexHtml = fs.readFileSync("./public/encode.html");
-  res.type("text/html").send(bufferIndexHtml);
+	res.view("/views/encode.ejs", { text: "text" });
 });
 
 fastify.get("/decode", function (req, res) {
-  const bufferIndexHtml = fs.readFileSync("./public/decode.html");
-  res.type("text/html").send(bufferIndexHtml);
+	res.view("/views/decode.ejs", { text: "text" });
 });
 
 fastify.get("/decodeFile", function (req, res) {
-  const bufferIndexHtml = fs.readFileSync("./public/keyStoreDecode.html");
-  res.type("text/html").send(bufferIndexHtml);
+	res.view("/views/keyStoreDecode.ejs", { text: "text" });
 });
-
 fastify.get("/logs", function (req, res) {
-  const bufferLogs = fs.readFileSync("requests.log");
-  res.type("application/json; charset=utf-8").send(bufferLogs);
+	const bufferLogs = fs.readFileSync("requests.log");
+	res.type("application/json; charset=utf-8").send(bufferLogs);
 });
 
 fastify.register(require("./api/decrypt"));
@@ -43,9 +43,9 @@ fastify.register(require("./api/authCheck"));
 fastify.register(require("./api/decodeFile"));
 
 fastify.listen(3000, function (err, address) {
-  if (err) {
-    fastify.log.error(err);
-    process.exit(1);
-  }
-  fastify.log.info(`Server listening on ${address}`);
+	if (err) {
+		fastify.log.error(err);
+		process.exit(1);
+	}
+	fastify.log.info(`Server listening on ${address}`);
 });
